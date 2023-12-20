ea0aa5de04273b36c5182eddabc52dd1779a5445
Mohammed Sabith E <108073559+Mohammed-Sabith-E@users.noreply.github.com>
2023-12-20 19:37:38 +0530

    Update aurifyview2.js

--- assets/js/core/aurifyview2.js
+++ assets/js/core/aurifyview2.js
@@ -292,7 +292,7 @@ async function showTable() {
             const newRow = document.createElement("tr");
             newRow.innerHTML = `
             <td style="text-align: right;">${metalInput}</td>
-            <td style="text-align: left; font-size:24px; font-weight: 600;">${purityInput}</td>
+            <td style="text-align: left; font-size:28px; font-weight: 600;">${purityInput}</td>
             <td>${unitInput} ${weightInput}</td>
             <td id="sellAED">0</td>
             <td id="buyAED">0</td>
